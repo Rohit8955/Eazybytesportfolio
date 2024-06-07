6b4e62d8590c8b5ad807ce3ec062b0633b43495f
Rohitsharma <rohitsharmaa971@gmail.com>
2024-06-07 16:43:23 +0530

    some changes

--- src/Components/skills/Skills.jsx
+++ src/Components/skills/Skills.jsx
@@ -10,10 +10,10 @@ const Skills = () => {
         </div>
 
         <div className='flex flex-col md:flex-row gap-6 items-center justify-center mt-3 md:mt-[30px]'>
-            <div className='w-[75%] h-[250px] md:w-[40%] border-gray-200 border-[2px] rounded-[10px]'>
+            <div className='w-[77%] h-[250px] md:w-[40%] border-gray-200 border-[2px] rounded-[10px]'>
                 <h1 className='text-center font-[500] md:text-[22px] text-[18px] mt-2'>Frontend Development</h1>
-                <div className='flex items-center justify-center mt-3'>
-                    <div className='grid grid-cols-2 md:gap-x-[150px]'>
+                <div className='flex items-center justify-center mt-3 md:mt-5'>
+                    <div className='grid grid-cols-2 md:gap-x-[120px]'>
                         <div className='flex flex-col text-[14px]'>
                             <div className='flex gap-3 items-center'><i className="fa-solid fa-circle-check text-black"></i>
                             <h1 className='font-[600] text-black'>HTML</h1></div>
@@ -47,9 +47,9 @@ const Skills = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-[75%] h-[250px] md:w-[40%] border-gray-200 border-[2px] rounded-[10px]'>
+            <div className='w-[77%] h-[250px] md:w-[40%] border-gray-200 border-[2px] rounded-[10px]'>
                 <h1 className='text-center font-[500] md:text-[22px] text-[18px] mt-2'>Backend Development</h1>
-                <div className='flex items-center justify-center mt-3'>
+                <div className='flex items-center justify-center mt-3 md:mt-5'>
                     <div className='grid grid-cols-2 md:gap-x-[150px]'>
                         <div className='flex flex-col text-[14px]'>
                             <div className='flex gap-3 items-center'><i className="fa-solid fa-circle-check text-black"></i>
