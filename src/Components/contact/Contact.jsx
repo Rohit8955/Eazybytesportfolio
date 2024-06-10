7431b70ce98e1ac55a9642979551e09e6bbd2448
Rohitsharma <rohitsharmaa971@gmail.com>
2024-06-10 20:56:03 +0530

    lst

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -4,7 +4,7 @@ import ContactSection2 from './ContactSection2'
 
 const Contact = () => {
   return (
-    <div id='contact' className='mt-[50px] md:mt-[100px]'>
+    <div id='contact' className='mt-[50px] md:mt-[100px] mb-[20px]'>
         <div className='flex flex-col items-center'>
             <h1 className='text-black font-[500] text-[24px] '>Contact Me</h1>
             <h1>Get in touch</h1>
